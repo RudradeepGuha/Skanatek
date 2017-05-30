d18e8cfa453fb099875720fa40cbc72387a5185b
RudradeepGuha <rudradeep4@gmail.com>
2017-05-30 14:18:58 +0530

    delete folder

--- Task 3/mysite/dnt/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    template = loader.get_template('dnt/index.html')
-    context = {'time': '10:10',}
-    return HttpResponse(template.render(context, request))
